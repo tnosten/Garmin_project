23b376a510087ed1973e45113414f2e77b741cbf
tnosten <tobias.nosten@gmail.com>
2025-02-15 22:23:02 +0100

    adding first files of project

---
+++ garmin_connect_test.py
@@ -0,0 +1,5 @@
+
+from garminconnect import Garmin
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
---
+++ setup.ps1
@@ -0,0 +1,11 @@
+# Create virtual environment
+python -m venv .venv
+
+# Activate virtual environment
+.venv\Scripts\Activate
+
+# Upgrade pip and install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+Write-Host "Setup complete. To activate the environment, run: .\.venv\Scripts\Activate"
