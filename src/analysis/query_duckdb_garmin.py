43183eae93fc40087d2cb2fbfd14a5063c702d4a
tnosten <tobias.nosten@gmail.com>
2025-03-04 15:45:11 +0100

    query dtabase

--- src/analysis/query_duckdb_garmin.py
+++ src/analysis/query_duckdb_garmin.py
@@ -3,6 +3,8 @@ import os
 from garminconnect import Garmin
 from dotenv import load_dotenv
 import duckdb
+import pandas as pd
+import streamlit as st
 
 # Load environment variables
 load_dotenv()
@@ -10,9 +12,28 @@ load_dotenv()
 ################# connect to  DuckDB
 conn = duckdb.connect("./data/garmin_pipeline.duckdb")
 
-# Run a simple query
-df = conn.execute("SELECT * FROM garmin_data.activities").fetchdf()
+# get all activities
+df = conn.execute("SELECT * FROM garmin_data.activities").fetchdf() 
+df.info()
+
+df.to_csv("output.csv", index=False)  # Save without index
+
+# Get the row with the highest "Split" values in seconds
+fastest_1k = df.loc[df["fastest_split_1000"].idxmax()]
+fastest_1mile = df.loc[df["fastest_split_1609"].idxmax()]
+fastest_5k = df.loc[df["fastest_split_5000"].idxmax()]
+print(fastest_1k)
+
+df.head(max())
 print(df)
+
+
+
+
+st.dataframe(all_activities) #save to to a streamlit dataframe
+
+
+
 
 
 ## drop table
