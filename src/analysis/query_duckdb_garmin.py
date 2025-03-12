3f2a9e846d6240c45fbf78030038a586017d4718
tnosten <tobias.nosten@gmail.com>
2025-03-12 23:51:24 +0100

    add map to the dashboard

--- src/analysis/query_duckdb_garmin.py
+++ src/analysis/query_duckdb_garmin.py
@@ -1,5 +1,4 @@
-import dlt
-import os
+
 from garminconnect import Garmin
 from dotenv import load_dotenv
 import duckdb
@@ -10,115 +9,64 @@ import streamlit as st
 load_dotenv()
 
 ################# connect to  DuckDB
-conn = duckdb.connect("./data/garmin_pipeline.duckdb")
 
 with duckdb.connect("./data/garmin_pipeline.duckdb") as conn:
     df = conn.execute("SELECT * FROM garmin_data.activities").fetchdf() # get all activities
 
 
 
-#explore data
-df.info()
-df.to_csv("output.csv", index=False)  # Save without index
+    #explore data
+    df.info()
+    df.to_csv("output.csv", index=False)  # Save without index
 
 
-# fastest 1K
+    best_efforts = conn.execute("""
+        WITH 
+        five_k_best AS (
+            SELECT 
+                '5K' AS best_effort, 
+                MIN(CAST(fastest_split_5000 AS float) / 60) AS time
+            FROM garmin_data.activities
+            LIMIT 1
+        ),
+        one_k_best AS (
+            SELECT 
+                '1K' AS best_effort, 
+                MIN(CAST(fastest_split_1000 AS float) / 60) AS time
+            FROM garmin_data.activities
+            LIMIT 1
+        ),
+        one_mile_best AS (
+            SELECT 
+                '1 mile' AS best_effort, 
+                MIN(CAST(fastest_split_1609 AS float) / 60) AS time
+            FROM garmin_data.activities
+            LIMIT 1
+        ),
+        Final as (
+            Select * from five_k_best
+            UNION
+            Select * from one_k_best
+            UNION
+            Select * from one_mile_best
+            )
+        SELECT * FROM Final
 
-fastest_1k = conn.execute("""
-    SELECT
-        activity_name, 
-        start_time_local AS datetime, 
-        MAX(CAST(fastest_split_1000 AS float)/60) AS fastest_1k 
-    FROM garmin_data.activities
-    GROUP BY activity_name, datetime
-    ORDER BY fastest_1k asc
-    limit 1
-""").fetchdf()
-
-print(fastest_1k)
-
-# fastest 1M
-
-
-fastest_1mile = conn.execute("""
-    SELECT
-        activity_name, 
-        start_time_local AS datetime, 
-        MAX(CAST(fastest_split_1609 AS float)/60) AS fastest_1mile 
-    FROM garmin_data.activities
-    GROUP BY activity_name, datetime
-    ORDER BY fastest_1mile asc
-    limit 1
-""").fetchdf()
-
-print(fastest_1mile)
-
-
-
-# fastest 5K
-
-fastest_5k = conn.execute("""
-    SELECT
-        activity_name, 
-        start_time_local AS datetime, 
-        MAX(CAST(fastest_split_5000 AS float)/60) AS fastest_5k 
-    FROM garmin_data.activities
-    GROUP BY activity_name, datetime
-    ORDER BY fastest_5k asc
-    limit 1
-""").fetchdf()
-
-print(fastest_5k)
+    """).fetchdf()
 
 
+    print(best_efforts)
 
-best_efforts = conn.execute("""
-    WITH 
-    five_k_best AS (
-        SELECT 
-            '5K' AS best_effort, 
-            MIN(CAST(fastest_split_5000 AS float) / 60) AS time
-        FROM garmin_data.activities
-        LIMIT 1
-    ),
-    one_k_best AS (
-        SELECT 
-            '1K' AS best_effort, 
-            MIN(CAST(fastest_split_1000 AS float) / 60) AS time
-        FROM garmin_data.activities
-        LIMIT 1
-    ),
-    one_mile_best AS (
-        SELECT 
-            '1 mile' AS best_effort, 
-            MIN(CAST(fastest_split_1609 AS float) / 60) AS time
-        FROM garmin_data.activities
-        LIMIT 1
-    ),
-    Final as (
-        Select * from five_k_best
-        UNION
-        Select * from one_k_best
-        UNION
-        Select * from one_mile_best
-        )
-    SELECT * FROM Final
-                            
-""").fetchdf()
+    st.title("My Garmin Activities")
+    st.dataframe(best_efforts)  # Displays in Streamlit
 
+    ## Create a sample DataFrame with latitude and longitude values
 
-print(best_efforts)
+    run_df = df[df["activity_type__type_key"] == "running"]
+    run_df = run_df[['start_latitude', 'start_longitude']]
 
-st.title("My DataFrame Viewer")
-st.dataframe(best_efforts)  # Displays in Streamlit
-
-
-
-
-
-## drop table
-
-# conn.execute("DROP SCHEMA IF EXISTS garmin_data CASCADE;")
-
-conn.close()
+    ## Create a map with the data    
+    run_df.columns = ['latitude', 'longitude']
+    ## Create a map with the data
+    st.map(run_df)
 
